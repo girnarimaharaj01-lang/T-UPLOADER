bc1b3a7ab85a05c2c730335b591c7a0842d3ec28
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-05-18 00:19:45 +0530

    Remove AUTH_USERS handling from vars.py
    Remove AUTH_USERS logic and related checks.

--- vars.py
+++ vars.py
@@ -6,10 +6,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8463395750:AAEZwDq7gVps8YZvfm0g6kLHftQI
 TOTAL_USER = os.environ.get('TOTAL_USERS', '8399557684').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8399557684').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
-    AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
