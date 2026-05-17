0965a53bffd4332b3ca0a1b4b5f012eccb5e0a2c
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-05-18 00:24:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,8 +4,6 @@ API_ID    = os.environ.get("API_ID", "29777466")
 API_HASH  = os.environ.get("API_HASH", "a04b3df726520026f207079aec2f9879")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8463395750:AAEZwDq7gVps8YZvfm0g6kLHftQIIKqv3qw") 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '8399557684').split(',')
-TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
-
-  
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]  
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
